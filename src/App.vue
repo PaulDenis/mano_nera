64fa15f253c57a8089c70a2ea697041cdc4eea3a
PaulDenis <78157298+PaulDenis@users.noreply.github.com>
2021-10-24 00:49:37 +0200

    Header and Navbar started. Animation for the Header completed.

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" class="logo">
     <!-- <HelloWorld msg="Welcome to La Mano Nera official website"/> -->
+    <Header />
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
     // HelloWorld
+    Header
   }
 }
 </script>
@@ -21,20 +24,27 @@ export default {
     box-sizing: border-box;
     margin: 0;
     padding: 0;
+  }
+
+  #app {
     background-color: black;
   }
 
-  @keyframes fade_in {
+@keyframes fade_in {
     from {opacity: 0;}
     to {opacity: 1;}
   }
 
   .logo {
-    height: 99vh;
+    height: 99.5vh;
     margin-left: 50%;
     transform: translateX(-50%);
     opacity: 1;
     animation-name: fade_in;
     animation-duration: 2s;
+  }
+
+  .active {
+    border-bottom: 2px solid #578d36;
   }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class=nav_bar>
+      <ul class="inline_menu">
+          <li class="active"><p>Home</p></li>
+          <li><p>About us</p></li>
+          <li><p>About our Members</p></li>
+          <li><p>Blog</p></li>
+          <li><p>Trade</p></li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style lang="scss" scoped>
+
+.nav_bar {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 75px;
+    background-color: rgba($color: #ffffff, $alpha: .05);
+    color: white;
+    animation-name: fade_in_extended;
+    animation-duration: 4s;
+    & > .inline_menu {
+        display: flex;
+        list-style: none;
+        width: 100%;
+        & > li {
+            padding: 25px;
+            height: 75px;
+            &:hover {
+                cursor: pointer;
+            }
+            &:hover > p {
+                transform: scale(1.2);
+                color: #578d36;
+
+            }
+        }
+    }
+}
+
+@keyframes fade_in_extended {
+    0% {
+        opacity: 0;
+    }
+    50% {
+        opacity: 0;
+    }
+    100% {
+        opacity: 100;
+    }
+  }
+
+</style>
