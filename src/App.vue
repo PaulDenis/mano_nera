153442e4c5dfbe997a71191241b55df34baf5273
PaulDenis <78157298+PaulDenis@users.noreply.github.com>
2021-10-23 23:46:39 +0200

    Animation for the logo created

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to La Mano Nera official website"/>
+    <img alt="Vue logo" src="./assets/logo.png" class="logo">
+    <!-- <HelloWorld msg="Welcome to La Mano Nera official website"/> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    background-color: black;
+  }
+
+  @keyframes fade_in {
+    from {opacity: 0;}
+    to {opacity: 1;}
+  }
+
+  .logo {
+    height: 99vh;
+    margin-left: 50%;
+    transform: translateX(-50%);
+    opacity: 1;
+    animation-name: fade_in;
+    animation-duration: 2s;
+  }
 </style>
